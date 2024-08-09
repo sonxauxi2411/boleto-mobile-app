3cdf7df79265e35d4022b413adcac23ea372245e
sonxauxi2411 <sontnfx18388@funix.edu.vn>
2024-08-09 18:40:17 +0700

    screen account not login

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,12 +1,52 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Image, StyleSheet, Text, View } from "react-native";
+import React from "react";
+import ButtonLinearGradient from "@/components/ButtonLinearGradient";
 
 export default function Profile() {
+
+  const handleNavigation = ()=>{
+
+  }
   return (
     <View>
-      <Text>Profile</Text>
+      {/* header */}
+      <View style={styles.header}>
+        <Text style={styles.title}> My Account</Text>
+      </View>
+     <View style={{backgroundColor : "#f6f8fa" , height: "100%" , width :"100%", paddingVertical: 20}}>
+     <View style={styles.cardLogin}>
+          <Image source={require("../../assets/images/avatar.png")} />
+          <View>
+            <ButtonLinearGradient title='Login' handleBtn={handleNavigation} />
+            <Text style={{textAlign:"center"}}>To fully experience features in</Text>
+          </View>
+      </View>
+     </View>
     </View>
-  )
+  );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  header: {
+    backgroundColor: "#0a1e5e",
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 10,
+    paddingTop: 35,
+  },
+  title: {
+    padding: 10,
+    color: "#fff",
+    fontSize: 20,
+  },
+  cardLogin :{
+    display : "flex", 
+    flexDirection : "row",
+    justifyContent : "space-evenly",
+    alignItems : "center",
+    marginBottom : 20,
+    paddingHorizontal : 10,
+    backgroundColor : "#fff",
+    borderRadius : 10,
+  }
+});
